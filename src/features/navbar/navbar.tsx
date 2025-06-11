802853d261d3158521c9307ddcef7c2f5333fa4e
alfindwi <alvindwi146@gmail.com>
2025-06-11 12:17:39 +0700

    first commit

--- src/features/navbar/navbar.tsx
+++ src/features/navbar/navbar.tsx
@@ -26,7 +26,7 @@ export function Navbar() {
       boxShadow="sm"
     >
       <Flex alignItems="center" as={Link} to="/" ml={5}>
-        <Img src="/src/assets/logo.svg" alt="Logo" width="50px" zIndex={2} />
+        <Img src="https://res.cloudinary.com/db2rr1kej/image/upload/v1749618908/logo_khch8q.png" alt="Logo" width="50px" zIndex={2} />
       </Flex>
 
       <Flex gap={8} mr={5} alignItems="center">
